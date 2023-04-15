3f9be88f5b4b2c4ec1cd681f8d2aac16b9832970
djambred <djambred@gmail.com>
2023-04-15 09:09:14 +0700

    fresh from oven

--- pert4/latihan3.cc
+++ pert4/latihan3.cc
@@ -3,8 +3,12 @@
 using namespace std;
 
 int main(){
-
-    
+    string nama, nim;
+    cout<<"masukkan nama : "<<endl;
+    cin>>nama;
+    cout<<"masukkan nim : "<<endl;
+    cin>>nim;
+    cout<<nama<<nim;
 
     return 0;
 }
---
+++ pert4/latihan4.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a, b, da, db;
+    cout <<"masukkan angka 1 : ";
+    cin >> a;
+    cout <<"masukkan angka 2 : ";
+    cin >> b;
+    cout<<"angka 1 : "<<a<<endl;
+    cout<<"angka 2 : "<<b<<endl;
+    da = a*2;
+    db = b*2;
+    cout<<"hasil double angka 1 : "<<da<<endl;
+    cout<<"hasil double angka 2 : "<<db<<endl;
+    return 0;
+}
